1741c4a967c336b00fd2e606d6cefc606080e568
fomoro <fomoro@gmail.com>
2023-05-07 21:26:30 -0500

    proyecto base de microservicios arquitectura limpia

---
+++ clean-service/src/main/java/com/linktic/infrastructure/conf/CleanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.infrastructure.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanServiceApplication.class, args);
+	}
+
+}
---
+++ clean-service/src/test/java/com/linktic/infrastructure/conf/CleanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.infrastructure.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
